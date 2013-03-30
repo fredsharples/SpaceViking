954be375862fa7cb6a3e295365b06c7a97755127
Fred Sharples <fred@orangedesign.com>
2013-03-30 11:37:52 -0700

    Added background layer

---
+++ Classes/BackgroundLayer.h
@@ -0,0 +1,16 @@
+//
+//  BackgroundLayer.h
+//  SpaceViking
+//
+//  Created by Fred Sharples on 3/30/13.
+//  Copyright (c) 2013 Fred Sharples. All rights reserved.
+//
+
+
+#import <Foundation/Foundation.h> 
+#import "cocos2d.h"
+
+
+@interface BackgroundLayer : CCLayer
+
+@end
---
+++ Classes/BackgroundLayer.m
@@ -0,0 +1,13 @@
+//
+//  BackgroundLayer.m
+//  SpaceViking
+//
+//  Created by Fred Sharples on 3/30/13.
+//  Copyright (c) 2013 Fred Sharples. All rights reserved.
+//
+
+#import "BackgroundLayer.h"
+
+@implementation BackgroundLayer
+
+@end
