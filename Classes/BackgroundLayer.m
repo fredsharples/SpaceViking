d563038f4a3bb0dfa89d007b503f13a372816072
Fred Sharples <fred@orangedesign.com>
2013-03-30 13:45:48 -0700

    loading the background into BackgroundLayer Class

--- Classes/BackgroundLayer.m
+++ Classes/BackgroundLayer.m
@@ -10,4 +10,28 @@
 
 @implementation BackgroundLayer
 
+-(id) init
+{
+	if( (self=[super init])) {
+        
+		// ask director for the window size
+		CGSize size = [[CCDirector sharedDirector] winSize];
+        
+		CCSprite *backgroundImage;
+		
+		if( UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad ) {
+			backgroundImage = [CCSprite spriteWithFile:@"background.png"];
+		} else {
+			backgroundImage = [CCSprite spriteWithFile:@"backgroundiPhone.png"];
+		}
+		backgroundImage.position = ccp(size.width/2, size.height/2);
+        
+		// add the label as a child to this Layer
+		[self addChild: backgroundImage z:0 tag:0];	
+	}
+	
+	return self;
+}
+
+
 @end
