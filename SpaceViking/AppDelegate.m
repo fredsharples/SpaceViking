8786fb11712bb50de6a639038d34956607402397
Fred Sharples <fred@orangedesign.com>
2013-03-30 18:10:13 -0700

    fixed appdelegate to load the new scene

--- SpaceViking/AppDelegate.m
+++ SpaceViking/AppDelegate.m
@@ -10,6 +10,8 @@
 
 #import "AppDelegate.h"
 #import "IntroLayer.h"
+#import "GameScene.h"
+
 
 @implementation MyNavigationController
 
@@ -47,7 +49,9 @@
 	if(director.runningScene == nil) {
 		// Add the first scene to the stack. The director will draw it immediately into the framebuffer. (Animation is started automatically when the view is displayed.)
 		// and add the scene to the stack. The director will run it when it automatically when the view is displayed.
-		[director runWithScene: [IntroLayer scene]];
+		[director runWithScene: [GameScene node]];
+        //FS changed this from scene to node. Works but not sure why or what the difference is.
+        
 	}
 }
 @end
